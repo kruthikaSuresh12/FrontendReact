2e602a799de628051602db194ac75cff57c641f3
Kruthika-S <kruthika1208@gmail.com>
2025-08-16 22:46:49 +0530

    small change in db

--- FrontendReact/Backend/db.js
+++ FrontendReact/Backend/db.js
@@ -5,10 +5,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'kruthi@1234',
-  database: process.env.DB_NAME || 'parking',
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
