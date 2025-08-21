2b3159092f693dcd99d6c152fd51d13341cd0fa7
kruthikaSuresh12 <kruthikasuresh12@gmail.com>
2025-08-21 23:30:25 +0530

    Update server.js

--- FrontendReact/Backend/server.js
+++ FrontendReact/Backend/server.js
@@ -46,7 +46,7 @@ const corsOptions = {
 };
 
 app.use(cors({
-  origin: "https://parkingsystem-iuq9.onrender.com",
+  origin: "*",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
